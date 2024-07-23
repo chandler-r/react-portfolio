9b5b99a7fc8182cebfd6190bf4b4656343349e54
Chandler <chandler.reyes123@gmail.com>
2024-07-22 22:01:24 -0400

    created home page and finished sidebar

---
+++ src/components/Home/index.js
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+import LogoTitle from '../../assets/images/lett_C.png';
+import './index.scss';
+
+const Home = () => {
+    
+
+    return(
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi, <br/> I'm
+                <img src={LogoTitle} alt='developer' />
+                handler
+                <br/>
+                Software Developer
+                </h1>
+
+                <h2>Backend Dev / Python Expert / Producer</h2>
+                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Home
